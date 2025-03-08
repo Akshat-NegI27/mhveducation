9038d76bede1080f747ece7d6a3040b347b262dd
Akshat-NegI27 <141574274+Akshat-NegI27@users.noreply.github.com>
2025-03-08 21:20:19 +0530

    Updated the project with new files and folders

---
+++ src/components/about-content/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css'; 
+
+
+
+
+
+function Content() {
+
+
+  return (
+    <div className="about-page-container">
+   
+
+
+    </div>
+  );
+}
+
+export default Content;
