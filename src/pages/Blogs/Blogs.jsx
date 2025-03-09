8c4f785107e01b307fddeff118746082852b60aa
Advik <advikbhatt24@gmail.com>
2025-03-09 21:01:14 +0530

    Co-authored-by: Akshat Negi <Akshat-NegI27@users.noreply.github.com>

---
+++ src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Blogs from '../../components/Blogs/Blogs'
+import Footer from "../../components/footer/footer";
+const Blogs = () => {
+  return (
+    <>
+    <Navbar/>
+    <Blogs />
+    <Footer/>
+    </>
+
+  )
+}
+
+export default About
