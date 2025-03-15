b1dffdb5da9f2d0974d94c39960f1e54be5e6025
Akshat-NegI27 <141574274+Akshat-NegI27@users.noreply.github.com>
2025-03-15 15:15:36 +0530

    FIX contact route✅

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
-import ContactContent from "../../components/contact-content/contactContent";
+import ContactContent from "../../components/contact-content/ContactContent";
 import Footer from "../../components/footer/footer";
 const Contact = () => {
   return (
