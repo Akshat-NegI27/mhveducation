dbabc998918dad3ed9fc8e05449aab67da715b9d
Advik <advikbhatt24@gmail.com>
2025-03-22 16:23:07 +0530

    dist removed ✅

--- dist/assets/coursesdetailContent-BaX9BkTc.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DCPMLPoq.js";const t=()=>s.jsx("div",{className:"hi-cxontainer",children:s.jsx("h1",{children:"Courses details is this PAGE"})});export{t as default};
--- dist/assets/vid1-BWS4E2J0.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/vid1-BUvGqgm3.mp4";export{s as v};
